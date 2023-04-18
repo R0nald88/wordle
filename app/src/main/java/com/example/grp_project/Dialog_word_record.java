d436cae917f6d789758595ca82f75635a96617d6
Hall0w0rb <92286963+Hall0w0rb@users.noreply.github.com>
2023-04-18 13:10:56 +0800

    Update README.md

--- app/src/main/java/com/example/grp_project/Dialog_word_record.java
+++ app/src/main/java/com/example/grp_project/Dialog_word_record.java
@@ -1,14 +1,18 @@
 package com.example.grp_project;
 
 import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.icu.text.AlphabeticIndex;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.grp_project.Storage.Record;
 
@@ -33,9 +37,25 @@ public class Dialog_word_record extends AppCompatActivity {
         lv_WordRecord = findViewById(R.id.lv_WordRecord);
         WordListAdapter adapter = new WordListAdapter(this, testArray);
         lv_WordRecord.setAdapter(adapter);
+        set_background();
     }
 
+    public void set_background() {
+        LinearLayout LL3 = findViewById(R.id.LL4);
+        SharedPreferences sharedPreferences=getSharedPreferences("sharedPerferenceKey",MODE_PRIVATE);
+        if (sharedPreferences.contains("CustomUriKey")) {
+            Drawable d = Drawable.createFromPath(sharedPreferences.getString("CustomUriKey", ""));
+            d.setAlpha(200);
+            LL3.setBackground(d);
+        }
+        if (sharedPreferences.contains("nightmodeKey")){
+            if(sharedPreferences.getBoolean("nightmodeKey",false)){
+                getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }else {
+                getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+            }
+        }}
     /*public Dialog_word_record(Dialog dialog) {
         this.dialog = dialog;
     }*/
